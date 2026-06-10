a9a19dda34d833dbd14cb369df6ad80d002cda22
Clarko <someclarko@proton.me>
2026-06-10 22:40:13 +0200

    feat: add Generic3DObject
    Concrete implementation of RendableObject thet can be used for generic 3D object

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg130398/GameLogic/Generic3DObject.java
@@ -0,0 +1,68 @@
+package it.unicam.cs.mpgc.rpg130398.GameLogic;
+
+import it.unicam.cs.mpgc.rpg130398.api.RendableObject;
+import it.unicam.cs.mpgc.rpg130398.api.ShadersSource;
+import it.unicam.cs.mpgc.rpg130398.api.Transform;
+import it.unicam.cs.mpgc.rpg130398.api.Vertex;
+
+public class Generic3DObject implements RendableObject, Transform {
+    private boolean isDirty;
+    Vertex[] vertices;
+    ShadersSource PreferdShader;
+
+    float[] pos = new float[3];
+    float[] rot = new float[3];
+
+    public Generic3DObject() {
+        isDirty = true;
+    }
+
+    @Override
+    public Vertex[] getObjectVertices () {
+        return vertices;
+    }
+    @Override
+    public void setObjectVertices (Vertex[] Vertices) {
+        vertices = Vertices;
+        isDirty = true;
+    }
+    @Override
+    public ShadersSource GetPreferdShader () {
+        return PreferdShader;
+    }
+    @Override
+    public void SetPreferdShader (ShadersSource ShadersSource) {
+        PreferdShader = ShadersSource;
+        isDirty = true;
+    }
+    @Override
+    public boolean isDirty() {
+        return isDirty;
+    }
+    @Override
+    public void clearDirty() {
+        isDirty = false;
+    }
+    @Override
+    public float[] getPosition() {
+        return pos;
+    }
+    @Override
+    public void setPosition(float[] position) {
+        if (position == null || position.length != 3)
+            throw new IllegalArgumentException("Position not valid");
+        pos = position;
+        isDirty = true;
+    }
+    @Override
+    public float[] getRotation() {
+        return rot;
+    }
+    @Override
+    public void setRotation(float[] rotation) {
+        if (rotation == null || rotation.length != 3)
+            throw new IllegalArgumentException("Position not valid");
+        rot = rotation;
+        isDirty = true;
+    }
+}
