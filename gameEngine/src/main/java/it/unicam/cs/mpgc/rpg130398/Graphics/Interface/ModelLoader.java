4b939a9f22f6603ac24e4a5948e777bf1e243ec7
Clarko <someclarko@proton.me>
2026-06-12 15:35:43 +0200

    feat: add ModelLoader interface
    ModelLoader specifies a class that can load 3D model vertices and triangle triplets
    from a local path relative to the project folder.

---
+++ gameEngine/src/main/java/it/unicam/cs/mpgc/rpg130398/Graphics/Interface/ModelLoader.java
@@ -0,0 +1,30 @@
+package it.unicam.cs.mpgc.rpg130398.Graphics.Interface;
+import it.unicam.cs.mpgc.rpg130398.api.Vertex;
+import java.io.IOException;
+/**
+ * Reads vertices and triangle triplets from a 3D model file.
+ *
+ */
+public interface ModelLoader {
+
+    /**
+     * @param relativePath path relative to the project root
+     */
+    void setPath(String relativePath);
+    /**
+     * @throws IOException if the file does not exist or cannot be read
+     * @throws UnsupportedOperationException if the file format is not supported
+     */
+    void read()  throws IOException, UnsupportedOperationException;
+    /**
+     * @return vertices read from the file,
+     * null if the file has not been read successfully
+     */
+    Vertex[] getVertices();
+    /**
+     * @return triangle triplets read from the file.
+     * Each group of 3 indices defines a triangle referencing vertices from getVertices().
+     * Returns null if the file has not been read successfully.
+     */
+    short[] getTriangleTriplets();
+}
