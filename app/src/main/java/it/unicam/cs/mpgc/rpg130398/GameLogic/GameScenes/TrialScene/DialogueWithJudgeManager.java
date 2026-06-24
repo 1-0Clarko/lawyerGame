a67d6d7c1b79fbdef0755d681bde93528c0b66eb
Clarko <someclarko@proton.me>
2026-06-24 17:34:27 +0200

    refactor: moved FlagsDisplayUI and QuestionButtonsUI inside Helper to be used on multiple scenes

--- app/src/main/java/it/unicam/cs/mpgc/rpg130398/GameLogic/GameScenes/TrialScene/DialogueWithJudgeManager.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg130398/GameLogic/GameScenes/TrialScene/DialogueWithJudgeManager.java
@@ -4,7 +4,6 @@ import it.unicam.cs.mpgc.rpg130398.GameLogic.GenericDialog;
 import it.unicam.cs.mpgc.rpg130398.GameLogic.JSON_DialogLoader;
 import it.unicam.cs.mpgc.rpg130398.api.Dialog;
 import it.unicam.cs.mpgc.rpg130398.api.DialogLoader;
-//Place holder
 public class DialogueWithJudgeManager {
 
     DialogueWithJudgeManager() {
